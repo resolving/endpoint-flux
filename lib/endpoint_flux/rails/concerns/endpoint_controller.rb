37283c6a174843502dd57414f06754b8da7b3935
Arturs Kreipans <arturs.kreipans@gmail.com>
2017-06-27 12:36:30 +0300

    Adds Rails concern

---
+++ lib/endpoint_flux/rails/concerns/endpoint_controller.rb
@@ -0,0 +1,32 @@
+module EndpointFlux
+  module Rails
+    module Concerns
+      module EndpointController
+        extend ActiveSupport::Concern
+
+        def present(namespace)
+          _, response = endpoint_for(namespace).perform(request_object)
+
+          headers.merge! response.headers
+          render json: response.body
+        end
+
+        def endpoint_for(namespace)
+          return namespace unless ::EndpointFlux.config.endpoints_namespace
+          (::EndpointFlux.config.endpoints_namespace + '/' + namespace).camelize.constantize
+        end
+
+        def request_object
+          ::EndpointFlux::Request.new(
+            headers: headers.merge('Authorization' => request.headers['authorization']),
+            params: endpoint_params
+          )
+        end
+
+        def endpoint_params
+          params.permit!.except(:controller, :action, :format).to_h.deep_symbolize_keys
+        end
+      end
+    end
+  end
+end
