df64198b5d9c545c137e43d97530c5a49af0d7d7
Arturs Kreipans <arturs.kreipans@gmail.com>
2017-07-10 13:53:09 +0100

    Removes helper specific code (#2)

--- lib/endpoint_flux/request.rb
+++ lib/endpoint_flux/request.rb
@@ -10,15 +10,6 @@ module EndpointFlux
     attr_accessor :headers
     attr_accessor :namespace
     attr_accessor :endpoint
-    attr_accessor :current_user_params
     attr_accessor :scope
-
-    def current_user
-      @current_user ||= begin
-        if current_user_params && current_user_params['id']
-          User[current_user_params['id']]
-        end
-      end
-    end
   end
 end
