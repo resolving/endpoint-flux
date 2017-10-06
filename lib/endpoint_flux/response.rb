bba225a4375c90dc90a37930b6f9055023c49d5d
Arturs Kreipans <arturs.kreipans@gmail.com>
2017-10-06 16:14:57 +0300

    Removes redirect (#5)

--- lib/endpoint_flux/response.rb
+++ lib/endpoint_flux/response.rb
@@ -5,7 +5,7 @@ module EndpointFlux
       @headers = headers
     end
 
-    attr_accessor :body, :headers, :redirect
+    attr_accessor :body, :headers
 
     def success?
       body[:status].between?(200, 209)
@@ -25,14 +25,6 @@ module EndpointFlux
 
     def not_found?
       body[:status] == 404
-    end
-
-    def redirected?
-      !!redirect
-    end
-
-    def redirect_to(*options)
-      redirect = options
     end
   end
 end
