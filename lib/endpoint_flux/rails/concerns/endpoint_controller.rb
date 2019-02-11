a31ca18c8fc238ab130f46e7ed9abaa2acc3dd8c
Vlad Dombrovskiy <vladdombrovskiy@Vlads-MacBook-Pro.local>
2019-02-11 07:44:11 +0200

    OSO-2000: added remote_ip to request object

--- lib/endpoint_flux/rails/concerns/endpoint_controller.rb
+++ lib/endpoint_flux/rails/concerns/endpoint_controller.rb
@@ -19,12 +19,23 @@ module EndpointFlux
         def request_object
           ::EndpointFlux::Request.new(
             headers: headers.merge('Authorization' => request.headers['authorization']),
+            remote_ip: remote_ip,
             params: endpoint_params
           )
         end
 
         def endpoint_params
           params.permit!.except(:controller, :action, :format).to_h.deep_symbolize_keys
+        end
+
+        def remote_ip
+          ip = request.remote_ip.to_s
+
+          if ip == '127.0.0.1'
+            ip = request.env['HTTP_X_FORWARDED_FOR']
+          end
+
+          ip
         end
       end
     end
