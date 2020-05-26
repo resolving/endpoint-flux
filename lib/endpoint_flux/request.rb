cd2c4321d11d1ce80a56a134b911bce8827b2a67
Victor Vinogradov <happy.jedi.g@gmail.com>
2020-05-27 02:20:47 +0300

    Add trace-id to Request object

--- lib/endpoint_flux/request.rb
+++ lib/endpoint_flux/request.rb
@@ -1,10 +1,11 @@
 module EndpointFlux
   class Request
-    def initialize(headers: {}, remote_ip: '', params: {}, namespace: nil)
+    def initialize(headers: {}, remote_ip: '', params: {}, namespace: nil, trace_id: nil)
       @headers = headers
       @remote_ip = remote_ip
       @params = params
       @namespace = namespace
+      @trace_id = trace_id || SecureRandom.hex(32)
     end
 
     attr_accessor :params
@@ -13,5 +14,6 @@ module EndpointFlux
     attr_accessor :namespace
     attr_accessor :endpoint
     attr_accessor :scope
+    attr_accessor :trace_id
   end
 end
